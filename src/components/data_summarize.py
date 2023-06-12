fd336e460b9417a13cc4ac641daa14f74cc6dce1
Anwesh Saha <anweshsaha21@iitk.ac.in>
2023-06-12 15:59:51 +0530

    Trying to get summarize to work

--- src/components/data_summarize.py
+++ src/components/data_summarize.py
@@ -1,49 +1,38 @@
 import os
-from data_transformation import gen_para_pdf, gen_para_doc, gen_para_txt
+from data_transformation import gen_para_pdf, gen_para_doc, gen_para_txt, gen_para_file
 import torch
 from transformers import pipeline
 import sys
 folder_path = os.getcwd()
 sys.path.append(folder_path)
 
-file_path = os.path.join(folder_path, 'src')
+# file_path = os.path.join(folder_path, 'src')
 
-hf_name = 'pszemraj/led-base-book-summary'
+# hf_name = 'pszemraj/led-base-book-summary'
 
-summarizer = pipeline(
-    "summarization",
-    hf_name,
-    device=0 if torch.cuda.is_available() else -1,
-)
+# summarizer = pipeline(
+#     "summarization",
+#     hf_name,
+#     device=0 if torch.cuda.is_available() else -1,
+# )
 
-def extract_text(file_path):
-    if file_path.endswith('.pdf'):
-        text = gen_para_pdf(file_path)
-        return text
-    elif file_path.endswith('.doc') or file_path.endswith('.docx'):
-        text = gen_para_doc(file_path)
-        return text
-    elif file_path.endswith('.txt'):
-        text = gen_para_txt(file_path)
-        return text
-    else:
-        raise ValueError("Unsupported file type.")
     
 file_name = '3_Hydrogen-Compressed-Natural-Gas_1.pdf"'
-file_path = os.path.join(file_path, file_name)
-extracted_text = extract_text(file_path)
+# file_path = os.path.join(file_path, file_name)
+extracted_text = gen_para_file(file_name)
+print(extracted_text)
     
-result = summarizer(
-           extracted_text,
-           min_length=8, 
-           max_length=256,
-           no_repeat_ngram_size=3, 
-           encoder_no_repeat_ngram_size=3,
-           repetition_penalty=3.5,
-           num_beams=4,
-           do_sample=False,
-           early_stopping=True,
-    )
+# result = summarizer(
+#            extracted_text,
+#            min_length=8, 
+#            max_length=256,
+#            no_repeat_ngram_size=3, 
+#            encoder_no_repeat_ngram_size=3,
+#            repetition_penalty=3.5,
+#            num_beams=4,
+#            do_sample=False,
+#            early_stopping=True,
+#     )
 
 
     
